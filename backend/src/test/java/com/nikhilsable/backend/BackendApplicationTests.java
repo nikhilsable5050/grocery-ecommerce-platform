29edf785240bff7cbbc97e7979303f1465eee6e4
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-20 13:57:41 +0530

    Added spring boot backend folder

---
+++ backend/src/test/java/com/nikhilsable/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
